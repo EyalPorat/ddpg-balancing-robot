c19d599b5a79ff3425d7e8f01266f0739f080f54
EyalPorat <eyal.a.porat@gmail.com>
2025-03-11 21:06:19 +0200

    updated config

--- python/src/balancing_robot/environment/balancer_env.py
+++ python/src/balancing_robot/environment/balancer_env.py
@@ -154,7 +154,7 @@ class BalancerEnv(gym.Env):
 
         # Check if reached stable state
         min_angle_for_stable = np.deg2rad(5)
-        min_angular_velocity_for_stable = np.deg2rad(5)
+        min_angular_velocity_for_stable = np.deg2rad(20)
         reached_stable = abs(self.state[0]) < min_angle_for_stable and abs(self.state[1]) < min_angular_velocity_for_stable
 
         # Additional info
