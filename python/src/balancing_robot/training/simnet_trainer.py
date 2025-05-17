45d6828e86f449a7291e07a5324491dfd1c1ab11
EyalPorat <eyal.a.porat@gmail.com>
2025-05-17 19:46:54 +0300

    retrained simnet with more data

--- python/src/balancing_robot/training/simnet_trainer.py
+++ python/src/balancing_robot/training/simnet_trainer.py
@@ -565,7 +565,7 @@ class SimNetTrainer:
                     )
                 )
 
-                non_extreme_weight = 0.1
+                non_extreme_weight = 0.05
 
                 # Apply weighting
                 weights[~extreme_samples] = non_extreme_weight
